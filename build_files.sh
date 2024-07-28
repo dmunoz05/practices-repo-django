eeb222d7f5a091a903042153690bf340b05dd400
= <daniel20025febrero29@gmail.com>
2024-07-28 00:20:22 -0500

    Change use python and version

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,6 @@
 
 pip install -r requirements.txt
 
-python3.9 manage.py collectstatic --noinput --clear
+# python3.9 manage.py collectstatic --noinput --clear
 
-# gunicorn ipx.wsgi
+gunicorn ipx.wsgi.py
